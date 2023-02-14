ec5e617330457861ac66a86423ebbbbdf824af82
MisterVal777 <MisterVal7@outlook.com>
2023-02-14 14:40:14 -0800

    Uploading project2 assignment

---
+++ Basic-Java-Script-Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+	function my_Function() {         //Defining a function and naming it.
+	var sentence = "JavaScript";     //Defining a variable and giving it a value
+	sentence += " is harder than HTML and CSS!"; //Defining a variable and giving it a value
+	document.getElementById("Concatenate").innerHTML = sentence; //concatenating the variables into the html elementFromPoint
+}
