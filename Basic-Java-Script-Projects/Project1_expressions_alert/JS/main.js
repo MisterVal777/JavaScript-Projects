e0f780d5426014ad03ceac7219e159c922d85379
MisterVal777 <MisterVal7@outlook.com>
2023-02-13 17:52:01 -0800

    First assignment submission

---
+++ Basic-Java-Script-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,11 @@
+var A = "I miss you" + " -Hayd'n", B = "I miss you more Hayd'n <br> -Ryan<br><br><br>", Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string<br><br>"; //Creating multible variables
+
+document.write(B); //Document.write method Using Variable B
+
+document.write(Sent1 + Sent2); //Concatenate Sent1 & Sent2
+
+window.alert(A); //alert window using Variable A
+
+document.write("See below for the answer to 7 times 2.<br>"); //Document write method
+
+document.write(7+7); //Expression adding 7+7
